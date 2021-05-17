9c7c5c80a8b0888f10d49e60dd4260972ec12bcf
otavio <otavio-dantas@live.com>
2021-05-17 19:41:47 -0400

    fix

--- serv-connector/app.py
+++ serv-connector/app.py
@@ -1,9 +1,1 @@
-from flask import Flask
-from flask.json import jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return jsonify({'statusCode': 200})
-
+# TODO: implement RabbitMq
