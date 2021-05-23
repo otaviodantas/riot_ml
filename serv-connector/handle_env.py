b83c8c3edc705854bfb54266f5837ff4e253d962
otavio <otavio-dantas@live.com>
2021-05-22 21:02:10 -0400

    refact: fazendo algumas mudanças na distribuição das pastas

--- serv-connector/handle_env.py
+++ serv-connector/handle_env.py
@@ -1,7 +1,10 @@
+import os
+from pathlib import Path
 from dotenv import load_dotenv, set_key, find_dotenv,dotenv_values
 
+PATH = Path('.env')
 
-def load(): return load_dotenv()
+def load(): return load_dotenv(dotenv_path=PATH)
 
 def change_value(key, value) -> None:
     set_key(key_to_set=key, value_to_set=value, dotenv_path=find_dotenv())
--- serv-connector/reciver.py
+++ serv-connector/reciver.py
@@ -1,2 +1,28 @@
-import pika
+import os
+from handle_env import load
+from pika import BlockingConnection, ConnectionParameters, channel
 
+load()
+
+def main():
+    connection = BlockingConnection(ConnectionParameters(os.environ.get('MSG_ADRESS')))
+    channel = connection.channel()
+
+    def callback():
+        #TODO: implementar consumo da menssagem
+        ...
+    
+    channel.basic_consume(
+        queue='Hello', 
+        on_message_callback=callback,
+        auto_ack=True)
+
+    print(' [*] Esperando messagem')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        ...
+    except:
+        ...
+ 
